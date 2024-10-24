b19b2a0d168219196cee01230e3daf7e6a9a84e9
nimisha-gj <nimisha@infraspec.dev>
2024-10-24 17:00:19 +0530

    feat: define response intefaces

---
+++ src/types/responses.ts
@@ -0,0 +1,12 @@
+import { ClientConfig } from "./clientConfig";
+
+export interface AddClientResponse {
+    success: boolean;
+    message: string;
+    config?: ClientConfig;
+}
+
+export interface RemoveClientResponse {
+    success: boolean;
+    message: string;
+}
