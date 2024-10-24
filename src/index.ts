cb1649430bd333895b8dfbcb3337334441aec0bf
nimisha-gj <nimisha@infraspec.dev>
2024-10-24 17:04:33 +0530

    feat: add support for client management for mikrotik wireguard

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { MikroTikWireGuardClientManager } from './lib/MikroTikWireGuardClientManager';
+import { MikroTikSSHClient } from "./lib/MikrotikSSHClient";
+
+export { MikroTikWireGuardClientManager, MikroTikSSHClient };
